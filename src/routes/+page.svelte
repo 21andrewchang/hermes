03a4e7fd7df7e19e4709029b5ca80348700f8732
Andrew Chang <21andrewch@gmail.com>
2026-01-07 10:44:23 -0800

    feat: vgu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -213,12 +213,37 @@
 </script>
 
 <div class="flex h-screen flex-col bg-white text-black">
-	<header class="mb-1 flex items-center gap-4 p-4">
-		<img src={nexus} alt="Nexus" class="h-8 w-8" />
-		<svg width="1" height="24" class="rotate-20 transform text-stone-300">
-			<line x1="0.5" y1="0" x2="0.5" y2="24" stroke="currentColor" stroke-width="1" />
-		</svg>
-		<div class="text-lg">NBK Property Management</div>
+	<header class="mb-1 flex items-center justify-between p-4">
+		<div class="flex flex-row items-center gap-4">
+			<img src={nexus} alt="Nexus" class="h-8 w-8" />
+			<svg width="1" height="18" class="rotate-20 transform text-stone-300">
+				<line x1="0.5" y1="0" x2="0.5" y2="24" stroke="currentColor" stroke-width="1" />
+			</svg>
+			<div class="text-md">NBK Property Management</div>
+		</div>
+		<div class="flex flex-row items-center gap-2">
+			<button
+				class="flex h-8 w-8 items-center justify-center rounded-full border border-stone-200 bg-white hover:bg-stone-50"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					fill="currentColor"
+					class="bi bi-bell"
+					viewBox="0 0 16 16"
+				>
+					<path
+						d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"
+					/>
+				</svg>
+			</button>
+			<div
+				class="flex h-8 w-8 items-center justify-center rounded-full border border-green-900 bg-green-800 text-sm text-white"
+			>
+				AW
+			</div>
+		</div>
 	</header>
 
 	<nav class="relative border-b border-stone-200" bind:this={navElement}>
@@ -336,8 +361,20 @@
 						<div
 							class="flex w-3/4 items-center gap-2 rounded-md border border-blue-700 bg-blue-50 p-4"
 						>
-							<span class="font-semibold text-blue-900"
-								>AI Suggestion: {currentSuggestion.action}</span
+							<span class="font-semibold text-blue-900 flex flex-row items-center gap-2">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									fill="currentColor"
+									class="bi bi-magic"
+									viewBox="0 0 16 16"
+								>
+									<path
+										d="M9.5 2.672a.5.5 0 1 0 1 0V.843a.5.5 0 0 0-1 0zm4.5.035A.5.5 0 0 0 13.293 2L12 3.293a.5.5 0 1 0 .707.707zM7.293 4A.5.5 0 1 0 8 3.293L6.707 2A.5.5 0 0 0 6 2.707zm-.621 2.5a.5.5 0 1 0 0-1H4.843a.5.5 0 1 0 0 1zm8.485 0a.5.5 0 1 0 0-1h-1.829a.5.5 0 0 0 0 1zM13.293 10A.5.5 0 1 0 14 9.293L12.707 8a.5.5 0 1 0-.707.707zM9.5 11.157a.5.5 0 0 0 1 0V9.328a.5.5 0 0 0-1 0zm1.854-5.097a.5.5 0 0 0 0-.706l-.708-.708a.5.5 0 0 0-.707 0L8.646 5.94a.5.5 0 0 0 0 .707l.708.708a.5.5 0 0 0 .707 0l1.293-1.293Zm-3 3a.5.5 0 0 0 0-.706l-.708-.708a.5.5 0 0 0-.707 0L.646 13.94a.5.5 0 0 0 0 .707l.708.708a.5.5 0 0 0 .707 0z"
+									/>
+								</svg>
+								AI Suggestion: {currentSuggestion.action}</span
 							>
 							<div class="ml-auto flex gap-2">
 								<button
