d93475a21afa21be5092b509c8ac6aef8ef889f2
Andrew Chang <21andrewch@gmail.com>
2026-01-16 10:05:53 -0800

    feat: message bubble vgu

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -416,8 +416,8 @@
 					>
 						<div
 							class={message.sender_type === 'ai'
-								? 'whitespace-pre-wrap text-sm text-stone-700'
-								: 'whitespace-pre-wrap rounded-2xl bg-stone-200 px-4 py-3 text-sm text-stone-800'}
+								? 'mb-6 leading-8 whitespace-pre-wrap text-sm text-stone-700'
+								: 'mb-6 leading-8 whitespace-pre-wrap rounded-2xl bg-stone-50 px-4 py-2 text-sm text-stone-800'}
 						>
 							{message.content}
 						</div>
